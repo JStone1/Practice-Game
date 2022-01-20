70c55f8618b23c0daa1c64698bd16442c4769ba0
JStone1 <62293789+JStone1@users.noreply.github.com>
2022-01-20 15:31:51 +0000

    Restored damage logic

--- Platform Game Prototype/Assets/Scripts/Damage.cs
+++ Platform Game Prototype/Assets/Scripts/Damage.cs
@@ -13,4 +13,12 @@ public class Damage : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
